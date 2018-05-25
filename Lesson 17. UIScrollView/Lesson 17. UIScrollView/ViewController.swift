73529081d1ae5e86971fa3e109f56b2f461f885e
Anton Rubenchick <anton.rubenchik@gmail.com>
2018-05-25 12:23:07 +0300

    Lesson 17. Full

--- Lesson 17. UIScrollView/Lesson 17. UIScrollView/ViewController.swift
+++ Lesson 17. UIScrollView/Lesson 17. UIScrollView/ViewController.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2018 Anton Rubenchik. All rights reserved.
 //
 //1+ Повторите то, что есть в видео.
-//2. Создать форму заполнения анкеты в свободной форме, анкета должна быть по высоте больше чем экран
-//3. Поместить форму из п2 на UIScrollVIew
+//2+ Создать форму заполнения анкеты в свободной форме, анкета должна быть по высоте больше чем экран
+//3+ Поместить форму из п2 на UIScrollVIew
 //4. Если клавиатуры выезжает (при заполнение TextField допустим) увеличить ScrollVIew на размер клавиатуры
 //
 //Дополнительно:
-//1. Добавить возможность загрузки фото с камеры или фотопленки
-//2. С помощью ScrollView реализовать листание фото
+//1+ Добавить возможность загрузки фото с камеры или фотопленки
+//2+ С помощью ScrollView реализовать листание фото
 
 import UIKit
 
-class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var firstLabel              = UILabel()
     var secondLabel             = UILabel()
@@ -24,6 +24,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
     var macImageView            = UIImageView()
     var titleLabel              = UILabel()
     var mainScrollView          = UIScrollView()
+    var footScrollView          = UIScrollView()
     var takePhotoButton         = UIButton()
     var chooseFromLibraryButton = UIButton()
     var nickImageView           = UIImageView()
@@ -42,18 +43,14 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         
         createStartElements()
         UIApplication.shared.statusBarView?.backgroundColor = .white
-        
-        
-        
+
         //createThreeImageAndScroll()
         //createScrollView()
         //testTwoLablels()
         
     }
 
-    
     //MARK: - Create Elements
-
     
     func createStartElements() {
         view.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1)
@@ -62,25 +59,30 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         mainScrollView = UIScrollView(frame: CGRect(x: 0, y: 54, width: Int(view.frame.width), height: Int(view.frame.height-54)))
         mainScrollView.contentSize = mainScrollView.frame.size
         mainScrollView.contentSize.height = 800
+        mainScrollView.delegate = self
         
         // common Function
         createTitleLabel()
         createTopView()
-        
         createMiddleView()
-        
+        createFootView()
         
         // Add Views on Scroll
         mainScrollView.addSubview(topView)
         mainScrollView.addSubview(middleView)
+        mainScrollView.addSubview(footScrollView)
         
         //Add all elements on View
         
         view.addSubview(titleLabel)
         view.addSubview(mainScrollView)
         
-        // create TextFields
-  
+    }
+    func newImageView(image: UIImage,position: CGRect) -> UIImageView {
+        let newImageView = UIImageView(image: image)
+        newImageView.frame = position
+        newImageView.contentMode = .scaleAspectFit //вписываем в отведенный размер
+        return newImageView
     }
     
     fileprivate func createTitleLabel() {
@@ -102,6 +104,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         newLabel.baselineAdjustment = .alignCenters
         return newLabel
     }
+    
     func newTextField(defaultText:String, position: CGRect, tag: Int)-> UITextField{
         let newTextField = UITextField()
         newTextField .frame = position
@@ -113,12 +116,15 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         newTextField .delegate = self
         return newTextField
     }
+    
     fileprivate func createTopView() {
         // create TopView's elements
         // add Photo
         let nickImage = UIImage(named: "normal")
         nickImageView = UIImageView(image: nickImage)
         nickImageView.frame = CGRect(x: 10, y: 10, width: 100, height: 100)
+        nickImageView.contentMode = .scaleAspectFill // не растягивать
+        nickImageView.clipsToBounds = true // вписывать в рамку
         nickImageView.layer.borderColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1).cgColor
         nickImageView.layer.cornerRadius = 50
         nickImageView.layer.borderWidth = 4
@@ -126,6 +132,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         // add Buttons
         takePhotoButton = createButton(text: "Take Photo", position: CGRect(x: 144, y: 10, width: 200, height: 44))
         chooseFromLibraryButton = createButton(text: "Choose From Library", position: CGRect(x: 144, y: 64, width: 200, height: 44))
+        chooseFromLibraryButton.addTarget(self, action: #selector(pressChooseButton), for: .touchUpInside)
         
         // Add TopViews
         topView.backgroundColor = .white
@@ -136,6 +143,28 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         topView.addSubview(takePhotoButton)
         topView.addSubview(chooseFromLibraryButton)
         
+    }
+    @objc func pressChooseButton() {
+        let nickImage = UIImagePickerController()
+        nickImage.delegate = self
+        nickImage.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        nickImage.allowsEditing = false
+        self.present(nickImage, animated: true)
+        {
+            //After it is complete
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if  let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            nickImageView.image = image
+            footScrollView.isHidden = false
+            
+        }
+        else {
+            //error
+        }
+        self.dismiss(animated: true, completion: nil)
     }
     
     fileprivate func createMiddleView() {
@@ -148,9 +177,9 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         let adressLabel = createLabel(text: "Adress", position: CGRect(x: 10, y: 177, width: 80, height: 44))
         let ageLabel = createLabel(text: "Age", position: CGRect(x: 10, y: 221, width: 80, height: 44))
         //createTextField
-        nameTextField = newTextField(defaultText: "Dmitry", position: CGRect(x: 110, y: 6, width: 300, height: 31), tag: 0)
+        nameTextField = newTextField(defaultText: "Natali", position: CGRect(x: 110, y: 6, width: 300, height: 31), tag: 0)
         phoneTextField = newTextField(defaultText: "+7 937 265-00-12", position: CGRect(x: 110, y: 50, width: 300, height: 31), tag: 1)
-        emailTextField = newTextField(defaultText: "ilon.mask@gmail.com", position: CGRect(x: 110, y: 94, width: 300, height: 31), tag: 2)
+        emailTextField = newTextField(defaultText: "portman@gmail.com", position: CGRect(x: 110, y: 94, width: 300, height: 31), tag: 2)
         cityTextField = newTextField(defaultText: "San Fransico", position: CGRect(x: 110, y: 138, width: 300, height: 31), tag: 3)
         adressTextField = newTextField(defaultText: "Lincoln str. 12", position: CGRect(x: 110, y: 182, width: 300, height: 31), tag: 4)
         ageTextField = newTextField(defaultText: "42", position: CGRect(x: 110, y: 226, width: 300, height: 31), tag: 5)
@@ -174,6 +203,39 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
         middleView.addSubview(cityTextField)
         middleView.addSubview(adressTextField)
         middleView.addSubview(ageTextField)
+    }
+    
+    fileprivate func createFootView() {
+        //create FootView
+        footScrollView = UIScrollView(frame: CGRect(x: Int(mainScrollView.frame.maxX / 3), y: 430, width: Int(mainScrollView.frame.maxX / 3), height: Int(mainScrollView.frame.maxY - 430 - 60)))
+        footScrollView.contentSize = footScrollView.frame.size
+        var imageViewRect = footScrollView.bounds
+        footScrollView.contentSize.width = footScrollView.frame.width * 4 // ширину зоны скролла увеличили на 4 фото
+        footScrollView.isPagingEnabled = true
+        footScrollView.isHidden = true
+        footScrollView.delegate = self
+        
+        // create new Image
+        let firstImage = UIImage(named: "portman4")
+        let secondImage = UIImage(named: "portman2")
+        let thirdImage = UIImage(named: "portman3")
+        let forthImage = UIImage(named: "portman1")
+        
+        let firstImageView = newImageView(image: firstImage!, position: imageViewRect)
+        
+        imageViewRect.origin.x += imageViewRect.size.width
+        let secondImageView = newImageView(image: secondImage!, position: imageViewRect)
+        
+        imageViewRect.origin.x += imageViewRect.size.width
+        let thirdImageView = newImageView(image: thirdImage!, position: imageViewRect)
+        
+        imageViewRect.origin.x += imageViewRect.size.width
+        let forthImageView = newImageView(image: forthImage!, position: imageViewRect)
+        
+        footScrollView.addSubview(firstImageView)
+        footScrollView.addSubview(secondImageView)
+        footScrollView.addSubview(thirdImageView)
+        footScrollView.addSubview(forthImageView)
     }
     
     func createButton(text: String, position:CGRect) -> UIButton{
@@ -200,8 +262,9 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
     //MARK: - delegate
     // вызывается во время начала прокрутки прокрутки
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        print("Start scroll")
-//        print(scrollView.contentOffset.x)
+        print("Start scroll")
+        print(scrollView.contentOffset.y)
+        print(mainScrollView.frame.origin.y)
 //        scrollView.alpha = 0.5
         
     }
@@ -281,10 +344,14 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegat
             case 2: cityTextField.becomeFirstResponder()
             case 3: adressTextField.becomeFirstResponder()
             case 4: ageTextField.becomeFirstResponder()
-            case 5: textField.resignFirstResponder()
+            case 5: do {
+                mainScrollView.contentOffset.y -= 150
+                textField.resignFirstResponder()
+                }
             default:
                 break
             }
+        if textField.tag > 1 && textField.tag < 5 { mainScrollView.contentOffset.y += 50 }
             return true
         }
 }
